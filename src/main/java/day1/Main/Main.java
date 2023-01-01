dc1b2a14698608ba229640221ec35f74306bb4ab
sduranel <101678290+sduranel@users.noreply.github.com>
2022-12-31 21:04:18 -0500

    Day3

--- src/main/java/day1/Main/Main.java
+++ src/main/java/day1/Main/Main.java
@@ -1,8 +1,10 @@
-package day1;
+package day1.Main;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import day1.Array.ListArray;
 
 public class Main {
 	public static void main(String[] args) {
--- src/main/java/day1/Rest/GetRestEndPoint.java
+++ src/main/java/day1/Rest/GetRestEndPoint.java
@@ -1,4 +1,4 @@
-package day1;
+package day1.Rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class GetRestEndPoint {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(GetRestEndPoint.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello() {
